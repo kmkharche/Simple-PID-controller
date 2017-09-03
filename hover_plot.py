3d508d0d7f100800c9a170d82c8ac42f1f048bfc
Ketan Kharche <ketan3393@gmail.com>
2017-09-03 10:32:07 -0700

    fix: Modify code to use PI controller

--- hover_plot.py
+++ hover_plot.py
@@ -1,19 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from p_controller import P_Controller
+from pi_controller import PI_Controller
 from quad1d_eom import ydot
 
 ##################################################################################
 ##################################################################################
-# This code is what will be executed and what will produce your results
-# For this quiz you need to set kp below to your desired value
-# Then modify p_controller.py to build out your P controller
-kp = 0.8
+kp = 0.76
+ki = 0.10
 ##################################################################################
 ##################################################################################
 
 # Simulation parameters
-N = 500 # number of simultion points
+N = 500 # number of simulation points
 t0 = 0  # starting time, (sec)
 tf = 30 # end time, (sec)
 time = np.linspace(t0, tf, N)
@@ -29,21 +27,24 @@ y = [0, 0]
 # Initialize array to store values
 soln = np.zeros((len(time),len(y)))
 
-# Create instance of P_Controller class
-p = P_Controller()
+# Create instance of PI_Controller class
+pi = PI_Controller()
 
 # Set the Kp value of the controller
-p.setKP(kp)
+pi.setKP(kp)
+
+# Set the Ki value of the controller
+pi.setKI(ki)
 
 # Set altitude target
 r = 10 # meters
-p.setTarget(r)
+pi.setTarget(r)
 
 # Simulate quadrotor motion
 j = 0 # dummy counter
 for t in time:
     # Evaluate state at next time point
-    y = ydot(y,t,p)
+    y = ydot(y,t,pi)
     # Store results
     soln[j,:] = y
     j += 1
@@ -66,7 +67,8 @@ plt.show()
 
 fig2 = plt.figure()
 ax3 = fig2.add_subplot(111)
-ax3.plot(time, p.u_p, label='u_p', linewidth=3, color = 'red')
+ax3.plot(time, pi.u_p, label='u_p', linewidth=3, color = 'red')
+ax3.plot(time, pi.u_i, label='u_i', linewidth=3, color = 'blue')
 ax3.set_xlabel('Time, (sec)')
 ax3.set_ylabel('Control Effort')
 h, l = ax3.get_legend_handles_labels()
